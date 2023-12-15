e850df9d5a3ea9747852334b1eea276d8fcb1f40
SStout2024 <142941851+SStout2024@users.noreply.github.com>
2023-12-14 23:02:35 -0800

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,23 +1,41 @@
+#This file was created by Sawyer Stout
+#Sources: Kidscancode, Chris Bradfield
+
 import pygame as pg
 from pygame.sprite import Sprite
 
 from pygame.math import Vector2 as vec
 import os
 from settings import *
+from random import choice, randrange
 
 # setup asset folders here - images sounds etc.
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'images')
 snd_folder = os.path.join(game_folder, 'sounds')
 
+#sheet/class to access all sprites
+class Spritesheet: 
+    def __init__(self, filename):
+        self.spritesheet = pg.image.load(filename).convert()
+
+    def get_image(self, x, y, width, height):
+        image = pg.Surface((width, height))
+        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        image = pg.transform.scale(image, (width // 2, height // 2))
+        return image
+
+
+
 class Player(Sprite):
     def __init__(self, game):
+        self._layer = PLAYER_LAYER
         Sprite.__init__(self)
         # self.image = pg.Surface((50, 50))
         # self.image.fill(GREEN)
         # use an image for player sprite...
         self.game = game
-        self.image = pg.image.load(os.path.join(img_folder, 'theBigBell.png')).convert()
+        self.image = pg.image.load(os.path.join(img_folder, 'theBell.png')).convert()
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (0, 0)
@@ -56,6 +74,7 @@ class Player(Sprite):
 
 class Platform(Sprite):
     def __init__(self, x, y, w, h, category):
+        self._layer = PLATFORM_LAYER
         Sprite.__init__(self)
         self.image = pg.Surface((w, h))
         self.image.fill(GREEN)
@@ -73,19 +92,56 @@ class Platform(Sprite):
                 self.speed = -self.speed
 
 class Mob(Sprite):
-    def __init__(self, x, y, w, h, kind):
-        Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(RED)
+    def __init__(self, game):
+        self._layer = MOB_LAYER
+        self.groups = game.all_sprites, game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image_up = pg.image.load(os.path.join(img_folder, 'theBell.png')).convert()
+        self.image_up.set_colorkey(BLACK)
+        self.image_up = pg.transform.scale(self.image_up, (50, 50))
+        self.image_down = pg.image.load(os.path.join(img_folder, 'theBell.png')).convert()
+        self.image_down.set_colorkey(BLACK)
+        self.image_down = pg.transform.scale(self.image_down, (50, 50))
+        self.image = self.image_up
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.kind = kind
-        self.speed = 0
-        if self.kind == "moving":
-           self.speed = 5
-        self.pos = vec(WIDTH/2, HEIGHT/2)
-
+        self.rect.centerx = choice([-100, WIDTH + 100])
+        self.vx = randrange(1, 4)
+        if self.rect.centerx > WIDTH:
+            self.vx *= -1
+        self.rect.y = randrange(HEIGHT / 2)
+        self.vy = 0
+        self.dy = 0.5
 
     def update(self):
-        pass
+        self.rect.x += self.vx
+        self.vy += self.dy
+        if self.vy > 3 or self.vy < -3:
+            self.dy *= -1
+        center = self.rect.center
+        if self.dy < 0: 
+            self.image = self.image_up
+        else:
+            self.image = self.image_down
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.rect.y += self.vy
+        if self.rect.left > WIDTH + 100 or self.rect.right < -100:
+            self.kill()
+
+#   def update(self):
+ #      pass
+
+#class Mob(Sprite):
+#    def __init__(self, x, y, w, h, kind):
+#        Sprite.__init__(self)
+#        self.image = pg.Surface((w, h))
+#        self.image.fill(RED)
+#        self.rect = self.image.get_rect()
+#        self.rect.x = x
+#        self.rect.y = y
+#        self.kind = kind
+#        self.speed = 0
+#        if self.kind == "moving":
+#           self.speed = 5
+#        self.pos = vec(WIDTH/2, HEIGHT/2)
